4f784cb16f29a2fd568a89a6c1ebb2ba4bbc7026
Denis Ostapovich <vofksden@gmail.com>
2022-12-19 22:33:03 +0300

    Coalesce - dev

--- MemoryManager/CoalesceAllocator.cpp
+++ MemoryManager/CoalesceAllocator.cpp
@@ -14,20 +14,51 @@ CoalesceAllocator::~CoalesceAllocator() { Allocator::~Allocator(); }
 
 void CoalesceAllocator::Init() {
   Allocator::Init();
-  ptr_ = VirtualAlloc(nullptr, kMegabyte, MEM_RESERVE | MEM_COMMIT,
-                      PAGE_READWRITE);
+
+  ptr_ = AddPage();
 }
 
-void CoalesceAllocator::Destroy() { Allocator::Destroy(); }
+void CoalesceAllocator::Destroy() {
+  Allocator::Destroy();
+
+  FreePages();
+}
 
 void* CoalesceAllocator::Alloc(size_t size) {
   Allocator::Alloc(size);
 
-  return nullptr;
+  PageHeader* page = (PageHeader*)ptr_;
+  BlockHeader* block = FindFreeBlock(size);
+  block->isEmpty_ = false;
+
+  RemoveFreeBlock(block);
+
+  if (!block->HasUnusedSpace(size)) {
+    return block->data_;
+  }
+
+  // Shrink unused space
+  block->size_ = block->size_ - size;
+
+  byte* newBlockAddress = block->data_ + block->size_;
+  BlockHeader* newBlock = new (newBlockAddress) BlockHeader();
+
+  newBlock->next_ = block->next_;
+  newBlock->previous_ = block;
+
+  AddFreeBlock(newBlock);
+
+  return block->data_;
 }
 
 bool CoalesceAllocator::Free(void* ptr) {
   Allocator::Free(ptr);
+
+  PageHeader* page = (PageHeader*)ptr_;
+  while (page->HasNext()) {
+    if (page->ContainsPtr(ptr)) return true;
+  }
+
   return false;
 }
 
@@ -36,3 +67,111 @@ void CoalesceAllocator::DumpStat() const {}
 
 void CoalesceAllocator::DumpBlocks() const {}
 #endif  // _DEBUG
+
+void* CoalesceAllocator::AddPage() {
+  void* ptr = VirtualAlloc(nullptr, kMegabyte, MEM_RESERVE | MEM_COMMIT,
+                           PAGE_READWRITE);
+  PageHeader* page = (PageHeader*)ptr;
+  page->ptr_ = (byte*)page + sizeof(PageHeader);
+
+  BlockHeader* block = (BlockHeader*)page->ptr_;
+  block->data_ = (byte*)block + sizeof(BlockHeader);
+  block->size_ = kMegabyte - sizeof(PageHeader) - sizeof(BlockHeader);
+
+  page->freeListHead_ = block;
+
+  return ptr;
+}
+
+void CoalesceAllocator::FreePage(void* ptr) {
+  VirtualFree(ptr, 0, MEM_RELEASE);
+}
+
+void CoalesceAllocator::FreePages() {
+  PageHeader* page = (PageHeader*)ptr_;
+  PageHeader* nextPage = nullptr;
+
+  // TODO: add leak check
+
+  while (page->next_) {
+    nextPage = page->next_;
+
+    FreePage(page);
+
+    page = nextPage;
+  }
+}
+
+void* CoalesceAllocator::SplitBlock(BlockHeader* block, int payloadSize) {
+  int unusedSpace = block->size_ - payloadSize;
+  block->size_ = payloadSize;
+  block->isEmpty_ = false;
+
+  BlockHeader* newBlock = new (block->data_ + payloadSize) BlockHeader();
+  newBlock->size_ = unusedSpace;
+  newBlock->data_ = (byte*)newBlock + sizeof(BlockHeader);
+  newBlock->previous_ = block;
+  newBlock->next_ = block->next_;
+  if (newBlock->next_) {
+    newBlock->next_->previous_ = newBlock;
+  }
+
+  block->next_ = newBlock;
+
+  AddFreeBlock(newBlock);
+
+  return block->data_;
+}
+
+void CoalesceAllocator::AddFreeBlock(BlockHeader* block) {
+  PageHeader* page = (PageHeader*)ptr_;
+
+  block->nextFreeBlock_ = page->freeListHead_;
+  page->freeListHead_->previousFreeBlock_ = block;
+  page->freeListHead_ = block;
+}
+
+void CoalesceAllocator::RemoveFreeBlock(BlockHeader* block) {
+  PageHeader* page = (PageHeader*)ptr_;
+
+  if (block == page->freeListHead_) {
+    page->freeListHead_ = block->nextFreeBlock_;
+
+    if (block->nextFreeBlock_) {
+      block->nextFreeBlock_->previousFreeBlock_ = nullptr;
+    }
+
+    return;
+  }
+
+  if (block->previousFreeBlock_) {
+    block->previousFreeBlock_->nextFreeBlock_ = block->nextFreeBlock_;
+  }
+
+  if (block->nextFreeBlock_) {
+    block->nextFreeBlock_->previousFreeBlock_ = block->previousFreeBlock_;
+  }
+}
+
+// Linear search with first fit strategy
+CoalesceAllocator::BlockHeader* CoalesceAllocator::FindFreeBlock(int size) {
+  PageHeader* currentPage = (PageHeader*)ptr_;
+  BlockHeader* currentBlock = currentPage->freeListHead_;
+
+  while (!currentBlock->Fits(size)) {
+    currentBlock = currentBlock->nextFreeBlock_;
+
+    if (currentBlock) {
+      continue;
+    }
+
+    if (!currentPage->HasNext()) {
+      currentPage->next_ = (PageHeader*)AddPage();
+    }
+
+    currentPage = currentPage->next_;
+    currentBlock = currentPage->freeListHead_;
+  }
+
+  return currentBlock;
+}
--- MemoryManager/CoalesceAllocator.hpp
+++ MemoryManager/CoalesceAllocator.hpp
@@ -22,13 +22,47 @@ class CoalesceAllocator final : public Allocator {
 
  private:
   struct BlockHeader final {
-    BlockHeader* left_ = nullptr;
-    BlockHeader* right_ = nullptr;
-    int blockSize_ = 0;
+    BlockHeader* previous_ = nullptr;
+    BlockHeader* next_ = nullptr;
+    BlockHeader* previousFreeBlock_ = nullptr;
+    BlockHeader* nextFreeBlock_ = nullptr;
+    size_t size_ = 0;
     byte* data_ = nullptr;
+    bool isEmpty_ = true;
+
+    bool Fits(int size) { return size <= size_; }
+    bool CoversWholeBlock(int size) {
+      return size_ == size || size_ - size < sizeof(BlockHeader) + 1;
+    }
+
+    bool HasUnusedSpace(int size) {
+      return size_ - size > sizeof(BlockHeader) + 1;
+    }
+
+    bool HasNext() { return next_; }
+  };
+
+  struct PageHeader final {
+    PageHeader* next_ = nullptr;
+    BlockHeader* freeListHead_ = nullptr;
+    void* ptr_ = nullptr;
+
+    bool HasNext() { return next_; }
+
+    bool ContainsPtr(void* ptr) {
+      return ptr > ptr_ && ptr < (byte*)ptr_ + kMegabyte;
+    }
   };
 
   void* ptr_;
+
+  void* AddPage();
+  void FreePage(void* ptr);
+  void FreePages();
+  void* SplitBlock(BlockHeader* block, int size);
+  void AddFreeBlock(BlockHeader* block);
+  void RemoveFreeBlock(BlockHeader* block);
+  BlockHeader* FindFreeBlock(int size);
 };
 
 }  // namespace memory_manager
